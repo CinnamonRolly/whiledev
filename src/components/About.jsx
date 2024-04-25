da897d043bdb6f07a90767fb416016642cc839f4
M <MungkornHorpanich@gmail.com>
2024-04-25 11:30:29 +0700

    Change my age in my portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
         <div data-aos="fade-left" className='md:w-[50%] w-[350px]'>
           <h4 className='text-center md:text-start font-bold md:pl-4 pt-5 text-[#147efb]'>ABOUT ME</h4>
           <h3 className='md:text-start text-center md:pl-4 font-bold text-2xl'>Hi! I'm A dedicated Web Developer based in Thailand!📍</h3>
-          <p className='md:pl-4 pt-3 text-center md:text-start text-[#767676]'>I'm a web developer based in the Thailand area. I'm 13 years old, and I'm passionate about full-stack application development because of the diversity of technology I get to learn and code. I've been coding since I was in fourth grade. I start coding in Python and take a break after learning for two months. 2021: I'm back with a full passion for coding and have tried a coding bootcamp. Then I started to be a self-taught programmer and learned full-stack development with the MERN stack.</p>
+          <p className='md:pl-4 pt-3 text-center md:text-start text-[#767676]'>I'm a web developer based in the Thailand area. I'm 14 years old, and I'm passionate about full-stack application development because of the diversity of technology I get to learn and code. I've been coding since I was in fourth grade. I start coding in Python and take a break after learning for two months. 2021: I'm back with a full passion for coding and have tried a coding bootcamp. Then I started to be a self-taught programmer and learned full-stack development with the MERN stack.</p>
         </div>
     </div>
   )
